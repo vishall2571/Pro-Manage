3e578b46f8eaecbb03eefc7b66fde01f53eb2668
vishal_007 <65078498+vishall2571@users.noreply.github.com>
2026-08-31 15:24:01 +0530

    Add DynatraceRouteTracker to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import SignUp from "./components/signup/SignUp";
 import Login from './components/login/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import Share from "./components/share/Share";
+import DynatraceRouteTracker from "./DynatraceRouteTracker";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <DynatraceRouteTracker />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
